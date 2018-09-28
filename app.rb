f6ad5d373591819c6ad11948d1e97720308fe83e
Leslie Stevens <flylady@mail.utexas.edu>
2018-09-28 16:37:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -33,9 +33,9 @@ class App < Sinatra::Base
     end
 
     get "/:operation/:number1/:number2" do
-      operation = params[:operation]
-      num1 = params[:number1].to_i
-      num2 = params[:number2].to_i
+      @operation = params[:operation]
+      @num1 = params[:number1].to_i
+      @num2 = params[:number2].to_i
       binding.pry
       #if operation == "add"
       #  num1 + num2
