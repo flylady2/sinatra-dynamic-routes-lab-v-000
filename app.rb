83fb1cd52ef4569019f4ab227f637914bfd649ae
Leslie Stevens <flylady@mail.utexas.edu>
2018-09-28 16:34:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -37,15 +37,15 @@ class App < Sinatra::Base
       num1 = params[:number1].to_i
       num2 = params[:number2].to_i
       #binding.pry
-      if operation == "add"
-        num1 + num2
-      elsif operation == "subtract"
-        num1 - num2
-      elsif operation == "multiply"
-        num1 * num2
-      elsif operation == "divide"
-        num1/num2
-      end
+      #if operation == "add"
+      #  num1 + num2
+      #elsif operation == "subtract"
+      #  num1 - num2
+      #elsif operation == "multiply"
+      #  num1 * num2
+      #elsif operation == "divide"
+      #  num1/num2
+      #end
     end
       # Write your code here!
 
